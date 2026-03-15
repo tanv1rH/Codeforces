1cf7bad39964a9bd8d18fd921f1751583ae47179
tanv1rH <139251661+tanv1rH@users.noreply.github.com>
2026-03-15 23:02:13 +0600

    Add files via upload

---
+++ (A) Calculating Function.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define nline '\n'
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long n;
+    cin >> n;
+    if (n % 2 == 0)
+        cout << n / 2;
+    else
+        cout << (n + 1) / 2 * (-1);
+}
